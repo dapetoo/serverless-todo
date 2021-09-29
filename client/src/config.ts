5d45a5ecf7216fef410a0b2f8de33ef0878b30cc
dapetoo <dapetoo@gmail.com>
2021-09-29 23:48:12 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'gulq2vhz80'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  // DONE: Create an Auth0 application and copy values from it into this map
+  domain: 'dev-49egqbq3.us.auth0.com',            // Auth0 domain
+  clientId: 'naJe4WbPzcwsAp0m6W88PVCAVZI2MetX',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
